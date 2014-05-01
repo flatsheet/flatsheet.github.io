307c2ced14862aa47c896b2a5fc772604abc9fe8
sethvincent <sethvincent@gmail.com>
2014-05-01 16:03:36 -0700

    cleanup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-var Flatsheet = require('flatsheet');
-
-var flatsheet = new Flatsheet();
-flatsheet.sheet('tcuxl49owsafl-jgp5qrta', getRows);
-
-function getRows (err, res) {
-  console.log(err, res);
-}
