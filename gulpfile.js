cb9af628e03e79eae96dc8d54d0d6fa33fd39875
sethvincent <sethvincent@gmail.com>
2014-05-01 15:37:21 -0700

    cleanup

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
-var pkg = require('./package.json');
-
-gulp.task('deploy', function () {
-  var opts = { remoteUrl: pkg.repository.url, branch: 'wat' };
-  gulp.src('dist/').pipe(deploy(opts));
-});
